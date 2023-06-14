b4c06395bfd2c2fe5585191089a86802138984d6
Luz De La Rosa <luzdelarosaramirez@gmail.com>
2023-06-14 16:41:27 -0400

    Close #15 Save client's info in the local storage

--- src/components/Loan.jsx
+++ src/components/Loan.jsx
@@ -1,13 +1,52 @@
-import Header from "./Header";
-import ClientForm from "./ClientForm";
-import AddLoan from "./AddLoan";
-import ClientLoans from "./ClientLoans";
+import React from "react";
+import { useState } from "react";
+import Header from "./header";
+import AddClient from "./addClient";
+import AddLoan from "./addLoan";
+import ClientLoans from "./clientLoans";
 
 function Loan() {
+  const [client, setClient] = useState({
+    name: "",
+    governmentId: "",
+    email: "",
+    phone: "",
+    note: "",
+  });
+
+  const handleClientInfoOnChange = (e) => {
+    let name, governmentId, email, phone, note;
+    // debugger;
+    switch (e.target.name) {
+      case "name":
+        name = e.target.value;
+        setClient({ ...client, name });
+        break;
+      case "governmentId":
+        governmentId = e.target.value;
+        setClient({ ...client, governmentId });
+        break;
+      case "email":
+        email = e.target.value;
+        setClient({ ...client, email });
+        break;
+      case "phone":
+        phone = e.target.value;
+        setClient({ ...client, phone });
+        break;
+      case "note":
+        note = e.target.value;
+        setClient({ ...client, note });
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
       <Header />
-      <ClientForm />
+      <AddClient client={client} onChange={handleClientInfoOnChange} onClient={setClient} />
       <AddLoan />
       <ClientLoans />
     </>
